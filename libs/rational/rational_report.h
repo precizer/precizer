1f75f86196cfc23ceb4ecfa8b0375215bb7fe100
Your Name <you@example.com>
2024-11-27 10:51:14 -0500

    The function to report an error without relying on dynamic memory

---
+++ libs/rational/rational_report.h
@@ -0,0 +1,11 @@
+/**
+ *
+ * @file ational_report.h
+ * @brief Prototypes of functions for report an error without relying on dynamic memory
+ *
+ */
+
+void report(
+	const char *msg,
+	size_t failed_size
+);
