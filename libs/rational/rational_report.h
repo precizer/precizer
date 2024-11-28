4b72ba7e954e8b132e2e6071c8d36cf4b76b44ad
Your Name <you@example.com>
2024-11-28 09:56:14 -0500

    The function to report an error without relying on dynamic memory

--- libs/rational/rational_report.h
+++ libs/rational/rational_report.h
@@ -1,11 +1,17 @@
 /**
  *
- * @file ational_report.h
+ * @file rational_report.h
  * @brief Prototypes of functions for report an error without relying on dynamic memory
  *
  */
 
-void report(
-	const char *msg,
-	size_t failed_size
+// Convenience macro to automatically include source file, function, and line information
+#define report(...) REPORT(__FILE__, __func__, __LINE__, __VA_ARGS__)
+
+void REPORT(
+	const char *,
+	const char *,
+	int,
+	const char *,
+	...
 );
