4b204bedddf1d0fe6665f97e992e25e48cf27cea
Your Name <you@example.com>
2024-12-18 19:09:56 -0500

    Library bugfixing

--- libs/testitall/echo.c
+++ libs/testitall/echo.c
@@ -1,68 +1,59 @@
 #include "testitall.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * Appends formatted text to a dynamically allocated buffer.
  * If the buffer is not large enough, it is automatically resized.
  * @param buffer Pointer to the buffer.
- * @param ... The first argument is the format string (like printf), 
+ * @param ... The first argument is the format string (like printf),
  *            followed by variable arguments for the format string.
  */
-void echo(
-	mem_char *,
-	const char *,
-	...
-) __attribute__((format(gnu_printf, 2, 3)));
 void echo(
 	mem_char *buffer,
 	const char *format,
 	...
 ){
+	char* result = NULL;
 	va_list args;
 	va_start(args, format);
+	int characters = vasprintf(&result, format, args);
+	va_end(args);
 
-	if (format == NULL)
+	if(characters < 0)
 	{
+		// Failure
 		return;
 	}
 
-	// Создаём новый формат, объединяя prefix и format
-	size_t size = buffer->length + strlen(format) + 1; // +1 для завершающего нуля
+	size_t former_length = buffer->length;
+	size_t shift = former_length;
+	size_t new_size = former_length + (size_t)characters + 1;
 
-	char *new_format = (char *)malloc(size * sizeof(char));
-	if (new_format != NULL)
+	if(former_length > 0)
 	{
-		// Объединяем prefix и format
+		shift -= 1;
+
+		if(new_size > 0)
+		{
+			new_size -=1;
+		}
+	}
+
+	if(SUCCESS == realloc_char(buffer,new_size))
+	{
 		if(buffer->length > 0)
 		{
-			strcpy(new_format, buffer->mem);
-		} else if (size > 0){
-			new_format[0] = '\0';
+			// Add new string to the buffer
+			memcpy(buffer->mem + shift, result, (size_t)characters * sizeof(char));
+
+			// Null termination of the string
+			buffer->mem[buffer->length - 1] = '\0';
 		}
-		strcat(new_format, format);
 	} else {
-		report("Memory allocation failed, requested size: %zu bytes",size * sizeof(char));
-		return;
+		report("Memory allocation failed, requested size: %zu bytes", new_size);
 	}
 
-	char *str = NULL;
-
-	// Используем vasprintf для выделения и форматирования строки
-	int bytes = vasprintf(&str, new_format, args);
-
-	free(new_format); // Освобождаем временный буфер
-
-	if (bytes > -1) {
-		// Copy str to buffer->mem
-		if(SUCCESS == realloc_char(buffer,(size_t)bytes + 1))
-		{
-			memcpy(buffer->mem, str, buffer->length);
-		}
-
-	} else {
-		report("Memory allocation failed, requested size: %zu bytes", bytes + 1);
-	}
-
-	free(str);
-
-	va_end(args);
+	// Temporary buffer cleanup
+	free(result);
 }
