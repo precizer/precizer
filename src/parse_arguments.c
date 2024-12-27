d5e55a5f7cd785dc4611ac950c3f51a2988f171d
Your Name <you@example.com>
2024-12-27 08:03:02 -0500

    Clarification in --help documentation

--- src/parse_arguments.c
+++ src/parse_arguments.c
@@ -109,7 +109,7 @@ static struct argp_option options[] = {
 	 "especially in automated scripts, as incorrect usage may lead " \
 	 "to loss of file checksums and metadata.\n",0 },
 	{"database",'d',"FILE",0,"Database filename. Defaults to ${HOST}.db, where HOST is the local hostname\n",0 },
-	{"check-level",'l',"LEVEL",0,"Select database validation level: 'quick' for basic structure check, 'full' (default) for comprehensive integrity verification\n",0 },
+	{"check-level",'l',"FULL|QUICK",0,"Select database validation level: 'quick' for basic structure check, 'full' (default) for comprehensive integrity verification\n",0 },
 	{ 0,0,0,0,"Compare databases options:",1},
 	{"compare",'c',0,0,"Compare two databases from different sources. Requires two additional arguments " \
 	 "specifying paths to database files, e.g.:\n" BOLD " --compare database1.db database2.db" RESET "\n",0 },
