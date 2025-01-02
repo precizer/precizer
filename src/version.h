3e8c8c7f0d6ae82644de8707fca2807b611ad93c
Your Name <you@example.com>
2025-01-02 16:34:49 -0500

    Version numbering has been changed to sequential numbering

--- src/version.h
+++ src/version.h
@@ -3,4 +3,19 @@
  *
  */
 #define APP_NAME "precizer"
-#define APP_VERSION "0.2"
+#define APP_VERSION 0002
+
+/**
+ * @brief Helper macro for stringification
+ */
+#define STRINGIFY(x) #x
+
+/**
+ * @brief Helper macro for double expansion to resolve macros
+ */
+#define TOSTRING(x) STRINGIFY(x)
+
+/**
+ * @brief Version as string constant
+ */
+#define APP_VERSION_STR TOSTRING(APP_VERSION)
