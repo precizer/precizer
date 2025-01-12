6df80a4cf0a77bfe3e24dc140f32e66baa9ae0b8
Your Name <you@example.com>
2025-01-12 12:47:05 -0500

    By default, SQLite creates a table automatically when attempting to INSERT into a non-existent table. This behavior can create chaos during application development. The default option needs to be disabled

--- src/db_migrate_from_0_to_1.c
+++ src/db_migrate_from_0_to_1.c
@@ -148,24 +148,19 @@ Return migrate_from_0_to_1(const char *db_file_path){
 	if(SUCCESS == status)
 	{
 		/* Set safety pragmas */
-		const char *pragmas[] = {
-			"PRAGMA journal_mode=WAL",
-			"PRAGMA synchronous=FULL",
-			"PRAGMA foreign_keys=ON",
-			"PRAGMA locking_mode=EXCLUSIVE",
-			NULL
-		};
+		const char *pragmas = "PRAGMA journal_mode=WAL"
+		           "PRAGMA strict = ON;"
+		           "PRAGMA synchronous=FULL"
+		           "PRAGMA foreign_keys=ON"
+		           "PRAGMA locking_mode=EXCLUSIVE";
 
-		for(const char **pragma = pragmas; *pragma && SUCCESS == status; pragma++)
+		rc = sqlite3_exec(db,pragmas,NULL,NULL,&err_msg);
+
+		if(SQLITE_OK != rc)
 		{
-			rc = sqlite3_exec(db,*pragma,NULL,NULL,&err_msg);
-
-			if(SQLITE_OK != rc)
-			{
-				slog(ERROR,"Failed to set pragma: %s, Error: %s\n",*pragma,err_msg);
-				sqlite3_free(err_msg);
-				status = FAILURE;
-			}
+			slog(ERROR,"Failed to set pragmas, Error: %s\n",err_msg);
+			sqlite3_free(err_msg);
+			status = FAILURE;
 		}
 	}
 
