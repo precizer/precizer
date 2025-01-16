15cb3c7bb030c13c4d644312d79cef8a14ecaa2d
Your Name <195144580+precizer@users.noreply.github.com>
2025-01-16 17:58:02 -0500

    Test improvements

--- tests/src/test0015.c
+++ tests/src/test0015.c
@@ -1,3 +1,17 @@
 #include "sute.h"
 
-// Main test runner
+/**
+ * Testing scenario 15
+ *
+ * Database upgrade testing:
+ * - Upgrade the database from version 0 to version 1
+ * - Run the program again to verify that the database is actually at version 1
+ * - Launch the program without specifying a database to ensure that a new database is created with the correct version
+ * - Run the program with the --compare parameter to compare databases when one of them has an older version - this should generate an appropriate error message
+ * - Run the database comparison again using the --compare parameter, but this time with the --update option. The database should be upgraded accordingly.
+*/
+Return test0015(void){
+    Return status = SUCCESS;
+
+    RETURN_STATUS;
+}
