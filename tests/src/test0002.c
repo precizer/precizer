7f4beeee931cc141402a694b098ba210f0646e6b
Dennis Razumovsky <195144580+precizer@users.noreply.github.com>
2025-01-27 16:03:11 -0500

    Tests improvements and enhancements

--- tests/src/test0002.c
+++ tests/src/test0002.c
@@ -2,33 +2,13 @@
 
 /**
  *
- * Create a database with the default name
+ *
  *
  */
 Return test0002(void){
 	/// The status that will be passed to return() before exiting.
 	/// By default, the function worked without errors.
 	Return status = SUCCESS;
-
-	// Get the output of an external program
-	const char *command = "export TESTING=true;cd ${TMPDIR};"
-	        "${BINDIR}/precizer tests/examples/diffs";
-
-	const char *filename = "templates/0002.txt";  // File name
-	const char *template = "%DB_NAME%";
-
-	const char *replacement = getenv("DBNAME");  // Database name
-
-	if(replacement == NULL)
-	{
-		echo(STDERR,"ERROR: The environment variable DBNAME is not set\n");
-		return(FAILURE);
-	}
-
-	ASSERT(SUCCESS == match_file_template(command,filename,template,replacement,0));
-
-	// Clean up test results
-	ASSERT(SUCCESS == external_call("rm \"${TMPDIR}/${DBNAME}\"",SUCCESS,false,false));
 
 	RETURN_STATUS;
 }
