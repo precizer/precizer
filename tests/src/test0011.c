5949950ce0a9daa635012e5203f911e4a23ad7a8
Dennis Razumovsky <195144580+precizer@users.noreply.github.com>
2025-02-18 17:07:34 -0500

    Some regular expressions had errors. Now they’re fixed.

--- tests/src/test0011.c
+++ tests/src/test0011.c
@@ -331,8 +331,8 @@ static Return test0011_8_readme(void)
 	command = "export TESTING=false;cd ${TMPDIR};"
 	        "${BINDIR}/precizer --update"
 	        " --db-clean-ignored --ignore=\"^.*/path2/.*\""
-	        " --ignore=\"diff2/.*\" --include=\"diff2/1/AAA/ZAW/A/b/c/.*\""
-	        " --include=\"diff2/path1/AAA/ZAW/.*\" tests/examples/diffs";
+	        " --ignore=\"^diff2/.*\" --include=\"^diff2/1/AAA/ZAW/A/b/c/.*\""
+	        " --include=\"^diff2/path1/AAA/ZAW/.*\" tests/examples/diffs";
 
 	const char *filename = "templates/0011_008.txt";
 
