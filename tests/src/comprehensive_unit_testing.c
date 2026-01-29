45fa356c765715a37be480713ebb54689b275dd0
Dennis Razumovsky <195144580+precizer@users.noreply.github.com>
2026-01-28 23:00:21 -0500

    Try to fix build with cmocka on macOS

--- tests/src/comprehensive_unit_testing.c
+++ tests/src/comprehensive_unit_testing.c
@@ -10,12 +10,6 @@ Return comprehensive_unit_testing(void)
 
 	#include "comprehensive_unit_and_system_testing.cc"
 
-	/* Mock only tecting in unit-mode so the linker wraps are active */
-
-	#ifndef EVIL_EMPIRE_OS
-	TEST(test0031,"Read error handling during hashing…");
-	#endif
-
 	run_external = prev_run_mode;
 
 	RETURN_STATUS;
