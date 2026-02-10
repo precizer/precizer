4c601efbfdf2b3f00dcce9c3bb240613744e7739
Dennis Razumovsky <195144580+precizer@users.noreply.github.com>
2026-02-09 23:51:40 -0500

    Added --dry-run=with-checksums mode
    Extend --dry-run with a new with-checksums mode.
    When --dry-run=with-checksums is used, files are read byte-by-byte and
    checksums are computed, but the database is still not created or modified.
    
    The default --dry-run behavior is unchanged (no checksum calculation)

--- src/show_elapsed.c
+++ src/show_elapsed.c
@@ -30,6 +30,8 @@ void show_elapsed(const TraversalSummary *summary)
 
 	static const char *const rate_na = "n/a";
 	static const char *const rate_less_than_1bps = "less than 1B/s";
+	const bool perform_file_hashing = config->dry_run == false
+	        || config->dry_run_with_checksums == true;
 
 	// Sum of per-file hashing time collected in sha512sum() during file_list().
 	long long int elapsed_ns = summary->total_hashing_elapsed_ns;
@@ -46,24 +48,29 @@ void show_elapsed(const TraversalSummary *summary)
 	(void)form_date_r(elapsed_ns,elapsed_string,sizeof(elapsed_string));
 
 	char hashed_string[50] = {0};
-	(void)bkbmbgbtbpbeb_r(summary->total_hashed_bytes,MAJOR_VIEW,hashed_string,sizeof(hashed_string));
-
+	const char *hashed = rate_na;
 	const char *rate = rate_na;
 	const char *suffix = "";
 
-	if(elapsed_ns > 0LL)
+	if(perform_file_hashing == true)
 	{
-		long double bytes_per_second = ((long double)summary->total_hashed_bytes * 1000000000.0L) / (long double)elapsed_ns;
+		(void)bkbmbgbtbpbeb_r(summary->total_hashed_bytes,MAJOR_VIEW,hashed_string,sizeof(hashed_string));
+		hashed = hashed_string;
 
-		if(bytes_per_second < 1.0L)
+		if(elapsed_ns > 0LL)
 		{
-			rate = rate_less_than_1bps;
-		} else {
-			size_t speed_value = (size_t)bytes_per_second;
-			rate = bkbmbgbtbpbeb(speed_value,MAJOR_VIEW);
-			suffix = "/s";
+			long double bytes_per_second = ((long double)summary->total_hashed_bytes * 1000000000.0L) / (long double)elapsed_ns;
+
+			if(bytes_per_second < 1.0L)
+			{
+				rate = rate_less_than_1bps;
+			} else {
+				size_t speed_value = (size_t)bytes_per_second;
+				rate = bkbmbgbtbpbeb(speed_value,MAJOR_VIEW);
+				suffix = "/s";
+			}
 		}
 	}
 
-	slog(EVERY,"Total runtime: %s, elapsed time: %s, hashed: %s, hashing rate: %s%s\n",total_runtime_string,elapsed_string,hashed_string,rate,suffix);
+	slog(EVERY,"Total runtime: %s, elapsed time: %s, hashed: %s, hashing rate: %s%s\n",total_runtime_string,elapsed_string,hashed,rate,suffix);
 }
