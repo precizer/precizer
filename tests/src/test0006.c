88fd62128532c635b9aaf55cf0abb931ffe45fca
Dennis Razumovsky <195144580+precizer@users.noreply.github.com>
2026-03-11 23:46:50 -0400

    Fix macOS build

--- tests/src/test0006.c
+++ tests/src/test0006.c
@@ -9,7 +9,7 @@ Return test0006(void)
 {
 	INITTEST;
 
-	const char *command = "echo -n";
+	const char *command = "printf '%s' ''";
 	ASSERT(SUCCESS == external_call(command,NULL,NULL,COMPLETED,ALLOW_BOTH));
 
 	RETURN_STATUS;
